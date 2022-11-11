58fc6f9b1ce103df2f1d030aca04b91873bb410f
Hadley Wickham <h.wickham@gmail.com>
2022-11-11 13:25:35 -0600

    Handle books without appendices

--- R/chapters.R
+++ R/chapters.R
@@ -2,6 +2,7 @@ chapter_types <- function(path = "_quarto.yml") {
   yaml <- yaml::read_yaml(path)
 
   chapters <- yaml$book$chapters
+
   df <- do.call("rbind", lapply(chapters, function(x) {
     if (is.character(x)) {
       tibble::tibble(path = x, type = "chapter")
@@ -13,7 +14,7 @@ chapter_types <- function(path = "_quarto.yml") {
     }
   }))
 
-  appendices <- yaml$book$appendices
+  appendices <- yaml$book$appendices %||% character()
   df <- rbind(df, tibble::tibble(path = appendices, type = "appendix"))
 
   setNames(df$type, tools::file_path_sans_ext(df$path))
